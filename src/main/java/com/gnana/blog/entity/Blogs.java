c4192b8131b9baf9cf5ca8d03bbb4da085801099
Gnanendra <gnanendra13112003@gmail.com>
2025-04-19 13:44:01 +0530

    commint for entity classes

---
+++ src/main/java/com/gnana/blog/entity/Blogs.java
@@ -0,0 +1,44 @@
+package com.gnana.blog.entity;
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Blogs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @JsonFormat(shape = Shape.STRING, pattern = "DD-MM-YY")
+    private Date createdAt;
+
+    @ManyToOne
+    private Users user;
+
+}
